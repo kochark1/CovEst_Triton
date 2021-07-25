f884ee6cc6ea0b21034678cd5dd5234886f8a932
Kocharlakota Kameswara <kameswara.kocharlakota@aalto.fi>
2021-07-26 00:15:39 +0300

    msl

--- param_save.m
+++ param_save.m
@@ -27,7 +27,7 @@ targetUser = 1; % u
 NR_vec = 2.^(-3:3)*1000;
 NQ_vec = 2.^(-3:3)*1000;
 
-mu = 1; % UL signal power
+mu_val = 1; % UL signal power
 lambda_DL = 10; %DL signal power
 pilotSequenceLength = number_of_users; %p
 alpha_R = 0.95;
@@ -37,7 +37,7 @@ Pb = diag(diag(Rb));
 
 % number_of_cells, number_of_users, number_of_antennas, targetCell, targetUser, ULresources, NR, rho, lambda, sigma_sqr, mu
 
-[Rmatrices, Rsum, Rsum_DL, R_sqrt_root, Qmatrix, Wmatrix, W_bar, SE_const, SE_const_DL, W_diag, W_bar_diag, SE_const_diag, SE_const_diag_DL] = ConfigRQWC(number_of_cells, number_of_users, number_of_antennas, targetCell, targetUser, mu, lambda_DL, alpha_R, Rb, pilotSequenceLength);
+[Rmatrices, Rsum, Rsum_DL, R_sqrt_root, Qmatrix, Wmatrix, W_bar, SE_const, SE_const_DL, W_diag, W_bar_diag, SE_const_diag, SE_const_diag_DL] = ConfigRQWC(number_of_cells, number_of_users, number_of_antennas, targetCell, targetUser, mu_val, lambda_DL, alpha_R, Rb, pilotSequenceLength);
 
 Phi = dftmtx(pilotSequenceLength);
 for l = 1:number_of_cells
