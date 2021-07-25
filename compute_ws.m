8e2321d6171d6a0ddf5b2c31afbd7c35d0a5995e
Kocharlakota Kameswara <kameswara.kocharlakota@aalto.fi>
2021-07-26 00:07:42 +0300

    msl

--- compute_ws.m
+++ compute_ws.m
@@ -1,4 +1,6 @@
-function compute_ws(k_input, TT)
+function compute_ws(path, interm_folder,out_folder, k_input, TT)
+    interm_folder=strcat(path, '/', interm_folder);
+    out_folder=strcat(path, '/', out_folder);
     load(strcat(interm_folder,'/RQWfile.mat'));
     Ws_for_each_k(NR_vec, NQ_vec, nq_set, R_sqrt_root, number_of_cells,...
         number_of_users, number_of_antennas, targetCell, k_input,...
