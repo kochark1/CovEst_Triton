43287958d7414bf99c4d6b569cbdcf243be28ed7
Kocharlakota Kameswara <kameswara.kocharlakota@aalto.fi>
2021-07-26 10:13:16 +0300

    msl

--- compute_num_sims.m
+++ compute_num_sims.m
@@ -6,9 +6,6 @@ function compute_num_sims(path, interm_folder,out_folder, block_ID, TT)
     nq_index = 1;
     for nq_sim = nq_set
         numSimulations;
-        if ZF_FLAG
-            numSimulations_ZF;
-        end
 
         nq_index = nq_index + 1;
     end
