434481f0dd78e1630923dd9b07845c7e9814c05c
Kocharlakota Kameswara <kameswara.kocharlakota@aalto.fi>
2021-07-26 10:09:53 +0300

    msl

--- compute_num_sims.m
+++ compute_num_sims.m
@@ -1,6 +1,7 @@
 function compute_num_sims(path, interm_folder,out_folder, block_ID, TT)
     interm_folder=strcat(path, '/', interm_folder);
     out_folder=strcat(path, '/', out_folder);
+    nq_set = [1,6];
     
     nq_index = 1;
     for nq_sim = nq_set
